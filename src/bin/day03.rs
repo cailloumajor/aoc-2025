e36e255802a923dc0a9d163150eb1170cb85ba78
Arnaud Rocher <arnaud.roche3@gmail.com>
2025-12-15 20:29:29 +0000

    Add day 3 part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -6,35 +6,52 @@ fn main() {
     let input_path = PathBuf::from_iter([env!("CARGO_MANIFEST_DIR"), "inputs", "day03.txt"]);
     let input = fs::read_to_string(input_path).expect("reading the input should not fail");
 
+    let total_part1: u64 = input
+        .lines()
+        .map(|l| max_bank_joltage::<2>(l.as_bytes()))
+        .sum();
+
+    eprintln!("Total joltage for part 1 is {total_part1}");
+
+    let total_part2: u64 = input
+        .lines()
+        .map(|l| max_bank_joltage::<12>(l.as_bytes()))
+        .sum();
+
+    eprintln!("Total joltage for part 2 is {total_part2}");
+}
+
+/// Process the maximum joltage of a battery bank, given the number of batteries to enable in the bank.
+fn max_bank_joltage<const BAT_COUNT: usize>(bank: &[u8]) -> u64 {
+    let mut joltage_bytes = [0u8; BAT_COUNT];
+
     // A mapping of the first found position by digit (ASCII).
     let mut digits_first_pos = BTreeMap::new();
 
-    let mut total = 0u64;
+    let mut start_bank_index = 0usize;
 
-    for line in input.lines() {
+    for (idx, elt) in joltage_bytes.iter_mut().enumerate() {
         digits_first_pos.clear();
 
-        // Ensure all characters are non-zero ASCII digits.
-        assert!(line.as_bytes().iter().all(|d| matches!(*d, b'1'..=b'9')));
+        let end_index = bank.len() - BAT_COUNT + idx;
 
-        // Do not take the last digit for tens.
-        let tens_digits = &line.as_bytes()[..line.len() - 1];
-        for (pos, digit) in tens_digits.iter().enumerate() {
+        for (pos, digit) in bank
+            .iter()
+            .enumerate()
+            .skip(start_bank_index)
+            .take_while(|(i, _)| *i <= end_index)
+        {
             digits_first_pos.entry(*digit).or_insert(pos);
         }
-        let (tens_digit, tens_position) = digits_first_pos
+        let (max_digit, position) = digits_first_pos
             .last_key_value()
-            .expect("a tens digit should have been found");
-
-        let units_digit = line.as_bytes()[tens_position + 1..]
-            .iter()
-            .max()
-            .expect("an units digit should have been found");
-
-        let joltage = (tens_digit - b'0') * 10 + (units_digit - b'0');
-
-        total += u64::from(joltage);
+            .expect("a digit should have been found");
+        *elt = *max_digit;
+        start_bank_index = *position + 1;
     }
 
-    eprintln!("Total joltage is {total}");
+    str::from_utf8(&joltage_bytes)
+        .expect("joltage value should be valid utf-8")
+        .parse()
+        .expect("parsing joltage value should not fail")
 }
